832f8629cf442d2b574817710e86e900c41293f8
Brian Clauser <pzljug@hotmail.com>
2019-09-24 12:18:53 -0400

    added Cucumber baseket class

---
+++ cucumbers.py
@@ -0,0 +1,44 @@
+"""
+Simple class modeling a cucumber basket.
+Example taken from AndyLPK247/tau-pytest-bdd/chapter-3/cucumbers.py
+Cucumbers may be added or removed from the basket.
+The basket has a maximum size, however.
+"""
+
+class CucumberBasket:
+    def __init__(self, initial_count=0, max_count=10):
+        if initial_count < 0:
+            raise ValueError("Initial cucumber basket count must not be a negative")
+        if max_count < 0:
+            raise ValueError("Max cucumber basket count must not be a negative")
+
+        self._count = initial_count
+        self._max_count = max_count
+
+    @property
+    def count(self):
+        return self._count
+
+    @property
+    def full(self):
+        return self.count == self.max_count
+
+    @property
+    def empty(self):
+        return self.count == 0
+
+    @property
+    def max_count(self):
+        return self._max_count
+
+    def add(self, count=1):
+        new_count = self.count + count
+        if new_count > self.max_count:
+            raise ValueError("Attempted to add too many cucumbers")
+        self._count = new_count
+
+    def remove(self, count=1):
+        new_count = self.count - count
+        if new_count < 0:
+            raise ValueError("Attempted to remove too many cucumbers")
+        self._count = new_count
